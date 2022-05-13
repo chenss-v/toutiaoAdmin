a7a075ba5aeef65d1cf654f306aea75751959660
chenss-v <2418640240@qq.com>
2022-05-13 21:26:23 +0800

    gx

--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -1,19 +1,111 @@
 <template>
   <div class="">
-      sz
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>个人设置</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <el-row>
+        <el-col :span="15">
+          <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="编号：">
+              {{ user.id }}
+            </el-form-item>
+            <el-form-item label="手机：">
+              {{ user.mobile }}
+            </el-form-item>
+            <el-form-item label="媒体名称:">
+              <el-input v-model="user.name"></el-input>
+            </el-form-item>
+            <el-form-item label="媒体介绍:">
+              <el-input type="textarea" v-model="user.intro"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱:">
+              <el-input v-model="user.email"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">保存</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+        <el-col :offset="2" :span="4">
+          <label for="file">
+            <el-avatar shape="square" :size="100" fit="cover" :src="user.photo"></el-avatar>
+            <p>点击修改头像</p>
+          </label>
+          <!-- <p @click="$refs.file.click()">点击修改头像</p> -->
+          <input id="file" type="file" hidden ref="file" @change="onFileChange()">
+        </el-col>
+      </el-row>
+    </el-card>
+
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      append-to-body>
+      <img width="150" :src="previewImage" alt="">
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
+import { getUserProfile } from '@/network/user'
+
 export default {
   name: 'SettingIndex',
   data () {
     return {
-
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      user: {
+        email: '',
+        id: null,
+        intro: '',
+        mobile: '',
+        name: '',
+        photo: ''
+      },
+      dialogVisible: false,
+      previewImage: ''
     }
   },
   components: {
 
+  },
+  created () {
+    this.loadUser()
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    },
+    loadUser () {
+      getUserProfile().then(res => {
+        this.user = res.data.data
+      })
+    },
+    onFileChange () {
+      const file = this.$refs.file
+      const blobData = window.URL.createObjectURL(file.files[0])
+      this.previewImage = blobData
+      this.dialogVisible = true
+      this.$refs.file.value = ''
+    }
   }
 }
 </script>
