982a0100cb9f55cf5e100dbfd61b6459951dba2d
chenss-v <2418640240@qq.com>
2022-06-08 16:44:17 +0800

    gx

--- src/network/request.js
+++ src/network/request.js
@@ -1,38 +1,73 @@
+// 引入axios
 import axios from 'axios'
+// 引入处理json数据的json-bigint
 import JSONbig from 'json-bigint'
-//   // 响应拦截
-//   instance.interceptors.response.use(res => {
-//     // console.log(res)
-//     return res.data ? res.data : res
-//   }, err => {
-//     return err
-//   })
+import router from '../router/router'
+import { Message } from 'element-ui'
 
-//   return instance(config)
-// }
+// 创建一个axios实例
 const request = axios.create({
-  baseURL: 'http://api-toutiao-web.itheima.net', // 最新接口地址
-  // baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
-  transformResponse: [function (data) {
+  // baseURL: 'http://ttapi.research.itcast.cn/'
+  baseURL: 'http://api-toutiao-web.itheima.net',
+
+  // 自定义处理json原始数据的方式
+  // 定义后端返回的原始数据的处理(处理一些数据太大 json处理不了的时候)
+  transformResponse: [function (data) { // data数据为原始数据 未经处理的json格式
+    // axios默认是通过json.perse来转换格式，这里使用jsonBig.perse转换
+    // 但后端返回的可能不是json格式字符串，jsonBig就会报错，通过try-catch捕获异常，异常时返回axios原始转换格式的data
     try {
       return JSONbig.parse(data)
-    } catch (err) {
+    } catch (error) {
       return data
     }
   }]
+
 })
 
-// 请求拦截
+// 请求拦截器interceptors === 每次请求 之前 在请求体中添加 用户基本信息
 request.interceptors.request.use(
   function (config) {
     const user = JSON.parse(window.localStorage.getItem('user'))
+    // 如果请求信息内没有user === 添加
     if (user) {
       config.headers.Authorization = `Bearer ${user.token}`
     }
+    // 最后经过return...之后 => 请求才真正发送出去
     return config
-  }, function (err) {
-    return Promise.reject(err)
   }
+  // 请求失败
+  // function (error) {
+  //   return Promise.reject(error)
+  // }
 )
 
+// 相应拦截器 === 当使用虚假user进入主页时 > 发送请求相应拦截 自动删除user 并返回login
+request.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  // 当请求状态码为 2xx 的走这里 === 必须 return 否则在请求的地方拿不到数据
+  return response
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  // 当请求码为 2xx 以外的走这里
+  const { status } = error.response
+  if (status === 401) { // 当请求错误 有reponse值 且response.status为401
+    window.localStorage.removeItem('user') // 清楚虚假user
+    router.push('./login') // 转到login
+    Message.error('登陆状态无效，请重新登陆') // 提示信息
+  } else if (status === 400) { // 按照请求码提示用户错误信息
+    Message.error('参数错误，请检查请求参数')
+  } else if (status === 403) {
+    Message({
+      type: 'warning',
+      message: '没有操作权限'
+    })
+  } else if (status >= 500) {
+    Message.error('服务器内部异常，请稍后重试')
+  }
+  return Promise.reject(error)
+})
+
+// 导出实例
 export default request
--- src/router/router.js
+++ src/router/router.js
@@ -13,58 +13,92 @@ import Fans from '@/views/fans/'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/home',
-      component: Layout,
-      children: [
-        {
-          path: '',
-          name: 'home',
-          component: Home
-        },
-        {
-          path: '/article',
-          name: 'article',
-          component: Article
-        },
-        {
-          path: '/publish',
-          name: 'publish',
-          component: Publish
-        },
-        {
-          path: '/image',
-          name: 'image',
-          component: Image
-        },
-        {
-          path: '/comment',
-          name: 'comment',
-          component: Comment
-        },
-        {
-          path: '/settings',
-          name: 'settings',
-          component: Settings
-        },
-        {
-          path: '/fans',
-          name: 'fans',
-          component: Fans
-        }
-      ]
-    }
-  ]
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: { title: '登陆' }
+  },
+  {
+    path: '/home',
+    // name: 'layout', // 当前路由有默认子路由 且不用加name
+    component: Layout,
+    children: [
+      {
+        path: '', // 空 为当前路由的默认路由
+        name: 'home',
+        component: Home
+      },
+      {
+        path: '/article',
+        name: 'article',
+        component: Article,
+        meta: { title: '内容管理' }
+      },
+      {
+        path: '/publish',
+        name: 'publish',
+        component: Publish,
+        meta: { title: '发布文章' }
+      },
+      {
+        path: '/image',
+        name: 'name',
+        component: Image,
+        meta: { title: '素材管理' }
+      },
+      {
+        path: '/comment',
+        name: 'comment',
+        component: Comment,
+        meta: { title: '评论管理' }
+      },
+      {
+        path: '/settings',
+        name: 'settings',
+        component: Settings,
+        meta: { title: '个人设置' }
+      },
+      {
+        path: '/fans',
+        name: 'fans',
+        component: Fans,
+        meta: { title: '粉丝管理' }
+      }
+    ]
+  }
+]
+
+const router = new Router({
+  routes
 })
+
+// 组件路由 = 前置路由守卫 === 未登录直接跳转login
+router.beforeEach((to, from, next) => {
+  console.log(to, from)
+  // 拿到本地的user用户数据
+  const user = JSON.parse(window.localStorage.getItem('user'))
+
+  // 判断to（要进入的位置） 不为login时，判断是否已有user 无则转到login
+  if (to.path !== '/login') {
+    if (user) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    next()
+  }
+})
+
+// 后置路由守卫 跳转成功更改doc.title
+router.afterEach((to, from) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  } else {
+    document.title = '今日头条后台管理系统'
+  }
+})
+
+export default router
